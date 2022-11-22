438bb033d8fb8f3834b789f87ee34b77f0aad8fd
nishaya raj <nishayaraj@gmail.com>
2022-11-22 17:44:16 -0600

    fixed bugs

--- utils/data/gameData.js
+++ utils/data/gameData.js
@@ -7,5 +7,25 @@ const getGames = () => new Promise((resolve, reject) => {
     .catch(reject);
 });
 
+const createGame = (game) => new Promise((resolve, reject) => {
+  fetch('http://localhost:8000/games', {
+    method: 'POST',
+    body: JSON.stringify(game),
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+    },
+  })
+    .then((resp) => resolve(resp.json()))
+    .catch(reject);
+});
+
+const getGameTypes = () => new Promise((resolve, reject) => {
+  fetch('http://localhost:8000/gametypes')
+    .then((response) => response.json())
+    .then(resolve)
+    .catch(reject);
+});
+
 // eslint-disable-next-line import/prefer-default-export
-export { getGames };
+export { getGames, createGame, getGameTypes };
